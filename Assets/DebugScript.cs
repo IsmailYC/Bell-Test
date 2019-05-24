797ed84edfa86d811222471e13f11f4bab228c11
YOUSSOUF ISMAIL CHERIFI <youcef.ismail.cherifi@gmail.com>
2019-05-24 15:26:20 +0100

    Adding a debugging script

---
+++ Assets/DebugScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitUntil(() => GameManager.instance != null);
+        Debug.Log(GameManager.instance.Position2PupilLab(transform.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
